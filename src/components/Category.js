7b5f35cb7902c695721a160bfbd25b5104bd77fb
Waris Haleem <43011742+iAmWaris97@users.noreply.github.com>
2022-10-01 03:23:10 +0430

    Create categories component

---
+++ src/components/Category.js
@@ -0,0 +1,9 @@
+const Category = () => (
+  <div className="categories">
+    <button type="button" className="check-status">
+      Check Status
+    </button>
+  </div>
+);
+
+export default Category;
